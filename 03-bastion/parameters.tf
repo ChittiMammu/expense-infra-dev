851989fcd37d8bd7aa0fa89b9c7db614942dd8d3
ChittiMammu <mammu.malluru@gmail.com>
2024-06-22 22:04:04 +0530

    bastion

--- 03-bastion/parameters.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_ssm_parameter" "public_subnet_ids" {
-  name  = "/${var.project_name}/${var.environment}/public_subnet_ids"
-  type  = "StringList"
-  value = join("," ,module.aws_vpc.public_subnet_ids) # converting list to string list
-}
